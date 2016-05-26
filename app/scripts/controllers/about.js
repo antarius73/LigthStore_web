0259c7294f1a68b8067514ce2189a336dc72136b
antarius73 <gjonik@gmail.com>
2016-05-26 16:10:08 +0200

    initialisation du projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lightStoreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lightStoreApp
+ */
+angular.module('lightStoreApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lightStoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lightStoreApp
+ */
+angular.module('lightStoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
